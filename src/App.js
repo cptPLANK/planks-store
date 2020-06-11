0eaa7693d3f10d533c01857316597526ca1806f7
cptPLANK <hello@cptplank.io>
2020-06-11 23:29:29 +0200

    added router, updated menu items as width router hoc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 import 'normalize.css/normalize.css';
 import HomePage from './pages/homepage/homepage.component';
+
+
+const HatsPage = () => (
+  <div>hats</div>
+);
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <switch>
+        <Route path='/' component={HomePage} exact/>
+        <Route path='/hats' component={HatsPage} />
+      </switch>
     </div>
   );
 }
